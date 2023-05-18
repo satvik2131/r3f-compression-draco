6f52d48e643a72fcc63d27e1edbfc7eff08990fd
Satvik Kushwaha <59243339+satvik2131@users.noreply.github.com>
2023-05-18 18:12:35 +0530

    demo show

--- src/Compression.js
+++ src/Compression.js
@@ -1,11 +1,11 @@
 import React, { useRef, useEffect } from "react";
 import { useLoader, useThree } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import { GLTFExporter, GLTFLoader } from "three-stdlib";
+import { DRACOLoader, GLTFExporter, GLTFLoader } from "three-stdlib";
 //gltf-transform
 import { WebIO } from '@gltf-transform/core';
 import { KHRONOS_EXTENSIONS } from '@gltf-transform/extensions';
-import { draco, resample, prune, dedup } from "@gltf-transform/functions";
+import { draco, resample, prune, dedup, textureCompress } from "@gltf-transform/functions";
 import { saveAs } from "file-saver";
 import { ObjectLoader } from "three";
 
@@ -32,9 +32,15 @@ const handleCompression = async (path, scene) => {
     try {
         const encoderModule = new window.DracoEncoderModule();
         const decoderModule = new window.DracoDecoderModule();
-        // console.log(window.DecoderModule);
+
+        const ioOptions = {
+            webIO: {
+                binary: true, // Set to true for binary glTF (.glb) output
+                json: false, // Set to false to disable JSON (.gltf) output
+            },
+        };
         // Configure I/O.
-        const io = new WebIO()
+        const io = new WebIO(ioOptions)
             .registerExtensions(KHRONOS_EXTENSIONS)
             .registerDependencies({
                 'draco3d.decoder': await decoderModule, // Optional.
@@ -43,7 +49,6 @@ const handleCompression = async (path, scene) => {
 
         // Read from URL.
         const document = await io.read(encodeURI(path));
-
         await document.transform(
             // Losslessly resample animation frames.
             resample(),
@@ -55,28 +60,25 @@ const handleCompression = async (path, scene) => {
             draco(),
         );
 
+
         //Write JSON
-        // const glbJson = await io.writeJSON(document);
-        // const jsonloader = new ObjectLoader();
-        // jsonloader.load(JSON.stringify(glbJson.json), function (obj) {
-        //     scene.add(obj);
-        // });
+        const glbJson = await io.writeBinary(document);
+        const loader = new GLTFLoader();
+        const dracoLoader = new DRACOLoader();
+        loader.setDRACOLoader(dracoLoader);
+        loader.parse(glbJson, '', (gltf) => {
+            console.log("huahua");
+            const gltfObject = gltf;
+            console.log(gltfObject);
+        });
 
-        // const loader = new ObjectLoader();
-        // console.log(glbJson);
-        // const gltf = loader.parse(glbJson.json, (res) => { console.log(res); });
-
-        // Write to byte array (Uint8Array).
-        const byteArrayGlb = await io.writeBinary(document);
-        const blob = new Blob([byteArrayGlb], { type: 'model/gltf+json' });
-        saveAs(blob, 'done.gltf');
-
-        // const url = URL.createObjectURL(blob);
 
     } catch (e) {
         console.log(e);
     }
 }
+
+
 
 
 
